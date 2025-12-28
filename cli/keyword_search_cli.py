7660ecac8e0fcd6e363244932a23cdd6ee08b50d
Mohammed Dadi <artofmohammed@gmail.com>
2025-12-28 08:13:49 +0100

    feat: add keyword search CLI with movie filtering
    - Create keyword_search_cli.py with search subcommand
    - Implement case-insensitive movie title matching

---
+++ cli/keyword_search_cli.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+from pathlib import Path
+import argparse, json
+
+
+def main() -> None:
+    parser = argparse.ArgumentParser(description="Keyword Search CLI")
+    subparsers = parser.add_subparsers(dest="command", help="Available commands")
+
+    search_parser = subparsers.add_parser("search", help="Search movies using BM25")
+    search_parser.add_argument("query", type=str, help="Search query")
+
+    args = parser.parse_args()
+
+    match args.command:
+        case "search":
+            movie_dataset_address = "data/movies.json"
+            print(f"Searching for: {args.query}")
+            matching_movies = match_movies(movie_dataset_address, args)
+            if not matching_movies:
+                return
+            max_iterations = 5
+            if len(matching_movies) < 5:
+                max = len(matching_movies)
+            for i in range(0, max_iterations):
+                print(f"{i+1}. {matching_movies[i]["title"]} {i+1}")  # 1. Movie Title 1
+        case _:
+            parser.print_help()
+
+
+def match_movies(file_address: str, parsed_args: argparse.Namespace) -> list:
+    # check if file exists
+    if not Path(file_address).exists():
+        print(f"broken file path: {file_address}")
+        return []
+
+    movie_title = parsed_args.query
+    matching_movies = []
+    with open(file_address, "r") as f:
+        movie_list = json.load(f)["movies"]
+        for movie in movie_list:
+            if movie_title.lower() in movie["title"].lower():
+                matching_movies.append(movie)
+
+    if not matching_movies:
+        print(f"no movies match the title: {movie_title}")
+        return []
+
+    matching_movies.sort(key=lambda x: x["id"])
+    return matching_movies
+
+
+if __name__ == "__main__":
+    main()
